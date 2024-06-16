800d8a3bbfe321cb644883e7727a2501db230787
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2024-06-16 22:28:24 +0300

    Update ScrollContext.tsx

--- frontend/src/app/[locale]/components/scroll-context/ScrollContext.tsx
+++ frontend/src/app/[locale]/components/scroll-context/ScrollContext.tsx
@@ -1,43 +1,3 @@
-// 'use client'
-// // ScrollContext.js
-// import { createContext, useContext, useRef } from 'react';
-
-// const ScrollContext = createContext(null);
-
-// export const ScrollProvider = ({ children }) => {
-//     const contactSectionRef = useRef(null);
-//     const headerOffset = 150; // Replace with your header's height
-
-//     const scrollToContact = () => {
-//         if (contactSectionRef.current) {
-//             const elementPosition = contactSectionRef.current.getBoundingClientRect().top;
-//             const offsetPosition = elementPosition + window.pageYOffset - headerOffset;
-
-//             window.scrollTo({
-//                 top: offsetPosition,
-//                 behavior: 'smooth'
-//             });
-//         }
-//     };
-
-//     return (
-//         <ScrollContext.Provider value={{ contactSectionRef, scrollToContact }}>
-//             {children}
-//         </ScrollContext.Provider>
-//     );
-// };
-
-// export const useScroll = () => {
-//     const context = useContext(ScrollContext);
-//     if (context === null) {
-//         return {
-//             contactSectionRef: { current: null },
-//             scrollToContact: () => console.warn("ScrollProvider is missing."),
-//         };
-//     }
-//     return context;
-// };
-
 'use client';
 // ScrollContext.js
 import React, { createContext, useContext, useRef } from 'react';
@@ -51,7 +11,7 @@ const ScrollContext = createContext<ScrollContextType | null>(null);
 
 export const ScrollProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const contactSectionRef = useRef<HTMLDivElement>(null);
-    const headerOffset = 150; // Replace with your header's height
+    const headerOffset = 120; // Replace with your header's height
 
     const scrollToContact = () => {
         if (contactSectionRef.current) {

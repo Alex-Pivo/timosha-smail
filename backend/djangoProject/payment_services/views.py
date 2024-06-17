0819fdbfa8993a777f0ec274c8bf5394fdf85f0c
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-06-17 20:31:03 +0200

    update

--- backend/djangoProject/payment_services/views.py
+++ backend/djangoProject/payment_services/views.py
@@ -5,7 +5,7 @@ import uuid
 
 from decimal import Decimal
 from datetime import datetime
-from . import settingstings
+from . import settings
 from .models import LiqpayPayment, InternationalPayment
 
 from liqpay.liqpay import LiqPay

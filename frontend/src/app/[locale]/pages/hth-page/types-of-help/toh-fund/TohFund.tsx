ddd39977ea20dea38da4499d301165f7042c8f4d
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2024-06-17 22:18:20 +0300

    Update TohFund.tsx

--- frontend/src/app/[locale]/pages/hth-page/types-of-help/toh-fund/TohFund.tsx
+++ frontend/src/app/[locale]/pages/hth-page/types-of-help/toh-fund/TohFund.tsx
@@ -1,14 +1,28 @@
+'use client'
 import styles from './TohFund.module.scss';
-import React from 'react'
+import React, {useEffect} from 'react'
 import Image from 'next/image'
 import { useTranslations } from 'next-intl';
 
 export const TohFund = () => {
+	useEffect(() => {
+		// Check if the URL contains the hash
+		if (window.location.hash) {
+			setTimeout(() => {
+				const yOffset = -200; // Adjust this value based on the height of your fixed header
+				const element = document.querySelector(window.location.hash);
+				if (element) {
+					const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
+					window.scrollTo({ top: y, behavior: 'smooth' });
+				}
+			}, 50); // Delay to ensure the page has loaded
+		}
+	}, []);
 	const t = useTranslations("htj");
 	return (
 		<section className={styles.typesOfHelp}>
-			<div className={styles.wrapper}>
-				<div className={styles.imageOfTypes} id="fund">
+			<div className={styles.wrapper} id="fundrais">
+				<div className={styles.imageOfTypes} >
 					<Image src={'/img8.png'} alt={'02'} width={666} height={401} className={styles.imgNote}/>
 					<Image src={'/image 9_Desc.png'} alt={'02'} width={830} height={500} className={styles.imgDesc}/>
 					<Image src={'/серце2.png'} alt={'02'} width={134} height={130} className={styles.hearts}/>
@@ -24,7 +38,7 @@ export const TohFund = () => {
 					</p>
 				</div>
 			</div>
-			<div className={styles.wrapperPhone}>
+			<div className={styles.wrapperPhone} id="fundrais">
 				<div className={styles.nameOfTypes}>
 					<div className={styles.name}>
 						<span>02</span>

d3eea61456f2c331ac8fb4c6b6b6287984d07896
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-06-17 22:35:18 +0300

    Update firstHelpCountry.tsx

--- frontend/src/app/[locale]/pages/requisites/firstHelpCountry.tsx
+++ frontend/src/app/[locale]/pages/requisites/firstHelpCountry.tsx
@@ -341,7 +341,7 @@ export default function FirstHelpCountry() {
                     setBtnEight((btnEight = false));
                     setBtnSix((btnSix = true));
                   }}
-                  className={btnSix || (amountPage !== "150" && amountPage !== "300" && amountPage !== "500" && amountPage !== "1000" && amountPage !== "5000") ? styles.blockActive : styles.block}
+                  className={btnSix || (amount === amountPage && (amountPage !== "150" && amountPage !== "300" && amountPage !== "500" && amountPage !== "1000" && amountPage !== "5000")) ? styles.blockActive : styles.block}
                 >
                   {t('another')}
                 </div>
@@ -350,7 +350,7 @@ export default function FirstHelpCountry() {
             <input
               type="text"
               placeholder={t('amount')}
-              className={btnSix || (amountPage !== "150" && amountPage !== "300" && amountPage !== "500" && amountPage !== "1000" && amountPage !== "5000") ? styles.inputActive : styles.input}
+              className={btnSix || (amount === amountPage && (amountPage !== "150" && amountPage !== "300" && amountPage !== "500" && amountPage !== "1000" && amountPage !== "5000")) ? styles.inputActive : styles.input}
               value={amount}
               onChange={(e) => setAmount(e.target.value)}
             />

0e89e6234108cd47d0e3560e52a0d488ddbcd81c
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-06-18 19:54:36 +0300

    Update page.tsx

--- frontend/src/app/[locale]/news/page.tsx
+++ frontend/src/app/[locale]/news/page.tsx
@@ -4,15 +4,15 @@ import Header from '@/app/[locale]/components/header/Header'
 import { Footer } from '@/app/[locale]/components/footer/Footer'
 import Articles from '@/app/[locale]/pages/news/articles'
 import stylesArticles from "../pages/news/styles/main.module.scss";
-import { getLocale } from "next-intl/server";
+import { useLocale } from "next-intl";
 import { get } from "lodash";
 
 export default function HowToHelpPage() {
-	let locale = getLocale();
+	let locale = useLocale();
 	return (
 		<>
 			<div className={styles.container}>
-				<Header />
+				<Header locale={locale} />
 			</div>
 			<div className={stylesArticles.body__container}>
 				<Articles locale={locale} />

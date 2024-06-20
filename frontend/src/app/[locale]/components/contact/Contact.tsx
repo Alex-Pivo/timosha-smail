b7da1e490ad53ecac9784472913a140f95827a0a
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2024-06-20 22:28:06 +0300

    Update Contact.tsx

--- frontend/src/app/[locale]/components/contact/Contact.tsx
+++ frontend/src/app/[locale]/components/contact/Contact.tsx
@@ -255,12 +255,16 @@ export const Contact = () => {
 							</button>
 						</div>
 						<div className={styles.btnsTablet}>
-									<select value={activeButton} className={styles.sel} onChange={handleSelectChange}>
+							<label htmlFor="btnsTabletSelect">
+								<div className={styles.select}>
+									<select value={activeButton} onChange={handleSelectChange}>
 										<option value="needHelp">{c('btn1')}</option>
 										<option value="vol">{c('btn2')}</option>
 										<option value="fund">{c('btn3')}</option>
 										<option value="friendship">{c('btn4')}</option>
 									</select>
+								</div>
+							</label>
 						</div>
 						{activeButton && (
 							<form className={styles.form} onSubmit={handleSubmit}>

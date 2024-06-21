c18dbd01c25a010569d39ab3247505c76dd8685b
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-06-21 23:05:35 +0300

    Update history.tsx

--- frontend/src/app/[locale]/pages/history-page/components/history.tsx
+++ frontend/src/app/[locale]/pages/history-page/components/history.tsx
@@ -142,7 +142,6 @@ export default function HistoryPart() {
                   </div>
                 </div>
                 </SwiperSlide>
-                <SwiperSlide />
               <BtnsHistory/>
             </Swiper>
           </div>

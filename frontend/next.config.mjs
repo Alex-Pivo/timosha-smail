cab2539122b94cb83e7d72eda72eb65704caa524
RAZZERGTI <78425187+RAZZERGTI@users.noreply.github.com>
2024-06-22 21:14:09 +0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,11 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['95.169.204.16'],
+    },
+};
 
 export default withNextIntl(nextConfig);

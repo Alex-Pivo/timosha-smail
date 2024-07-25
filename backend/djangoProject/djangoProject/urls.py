247ed0d1cc25b88f4c0a4a5aa1091d0c86c91aa3
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-07-25 12:38:09 +0200

    Update urls.py

--- backend/djangoProject/djangoProject/urls.py
+++ backend/djangoProject/djangoProject/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
 
@@ -15,5 +17,7 @@ urlpatterns = [
     path('email/', include('email_data.urls')),
     # path('fund/', include('pages.urls')),
 
-
+    #---------------errors--------------
+    # path('404/<str:language>', error_handler.ErrorView.as_view(), name='error_page'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+

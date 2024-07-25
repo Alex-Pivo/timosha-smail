d723c0d809664174ec33bc5086d9efbbb30e29a9
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-07-25 14:11:16 +0300

    Update slider.tsx

--- frontend/src/app/[locale]/pages/main-page/first-part/pages/slider.tsx
+++ frontend/src/app/[locale]/pages/main-page/first-part/pages/slider.tsx
@@ -30,7 +30,7 @@ export default function Slider({ locale }: any) {
     try {
       let data;
       axios
-        .get(`https://95.169.204.16:8000/${localeValue}/`)
+        .get(`https://95.169.204.16:8000/${localeValue}`)
         .then((res) => {
           data = res.data;
           setState(data);

fb69ed8156ae4c4e83d1c827a190b29aeb937897
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-07-25 14:09:48 +0200

    Update settings.py

--- backend/djangoProject/djangoProject/settings.py
+++ backend/djangoProject/djangoProject/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 

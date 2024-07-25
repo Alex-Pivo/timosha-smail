bdb80a80275cbd227379f470ed00862164b5c762
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-07-25 14:05:35 +0200

    Update settings.py

--- backend/djangoProject/djangoProject/settings.py
+++ backend/djangoProject/djangoProject/settings.py
@@ -191,7 +191,7 @@ USE_TZ = True
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type

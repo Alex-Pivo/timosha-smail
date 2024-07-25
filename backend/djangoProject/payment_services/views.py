a59e827fed93a6150e7343a671aa1e5a4554d95e
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-07-26 01:54:06 +0200

    Update views.py

--- backend/djangoProject/payment_services/views.py
+++ backend/djangoProject/payment_services/views.py
@@ -197,8 +197,8 @@ class LiqPayFunc:
             'currency': (currency if currency else 'UAH'),
             'description': ('Підтримка з сайту' if language =='uk' else 'Support from timoshas-smile.org'),
             'order_id': order_id,
-            'server_url': f'http://95.169.204.16:8000/{language}/status/{hashed_order_id}',
-            'result_url': f'http://95.169.204.16:3002/{new_language}/donate/status/{hashed_order_id}',
+            'server_url': f'timoshas-smile.org:8443/{language}/status/{hashed_order_id}',
+            'result_url': f'timoshas-smile.org/{new_language}/donate/status/{hashed_order_id}',
         }
 
         if is_subscription == 'true':

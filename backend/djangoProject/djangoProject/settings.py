fa5b9da142e15fb250158a42f42cfe3941113d17
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-07-25 11:03:12 +0200

    Update settings.py

--- backend/djangoProject/djangoProject/settings.py
+++ backend/djangoProject/djangoProject/settings.py
@@ -125,7 +125,7 @@ ROOT_URLCONF = 'djangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'djangoProject/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -187,13 +187,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",]
 #
 # settings.py
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'djangoProject/templates'
 
 
 

7ce19ebe0aa32c3925c60d47c18dff2385b70248
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-07-26 21:32:37 +0200

    Update urls.py

--- backend/djangoProject/payment_services/urls.py
+++ backend/djangoProject/payment_services/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name = 'payment_services'
 
 urlpatterns = [
-    # path('<str:language>', views.DonatePage.as_view(), name='donate_page'),
+
     path('<str:language>/', views.DonateView.as_view(), name='donate'),
     path('<str:language>/status/<str:hashed_order_id>', views.LiqPaymentAPI.as_view(), name='pay_callback'),
-    path('internation_payment/',views.InternationalPaymentInformation.as_view(),name='international_payment'),
+
 ]
-# zminiti na <str:language>
+

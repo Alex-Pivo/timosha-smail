dfe9d509b892813a01834125b5798bb1d673ec29
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-07-26 23:17:35 +0300

    Update page.tsx

--- frontend/src/app/[locale]/donate/status/[id]/page.tsx
+++ frontend/src/app/[locale]/donate/status/[id]/page.tsx
@@ -31,7 +31,7 @@ export default function Status() {
     // Функция для выполнения GET-запроса по указанной ссылке
     const getOrderStatus = async () => {
         try {
-            const response = await axios.get(`http://95.169.204.16:8000/donate/uk/status/${orderId}`);
+            const response = await axios.get(`https://timoshas-smile:8443/donate/${locale}/status/${orderId}`);
             console.log('Order Status:', response.data);
             if (response.data.status === 'success') {
                 setStatus(status = "true");

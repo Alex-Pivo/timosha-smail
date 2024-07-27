a53b5ec91651eda4a288b3f0b57ad75173562a35
Yuriy Klyap <126515747+cl9bix@users.noreply.github.com>
2024-07-27 12:26:52 +0200

    Update views.py

--- backend/djangoProject/payment_services/views.py
+++ backend/djangoProject/payment_services/views.py
@@ -204,7 +204,7 @@ class LiqPayFunc:
             params.update({
                 'action': 'subscribe',
                 'recurringbytoken': '1',
-                'phone': phone,
+                'phone': '+380956103761',
                 'subscribe_date_start': todays_date,
                 'subscribe_periodicity': 'month',
             })

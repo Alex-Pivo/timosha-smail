5f1f0be92da4e7e1204eecaed0dfbd7725ef3230
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-07-29 14:49:43 +0300

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,16 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
+  }
+};
 
 export default withNextIntl(nextConfig);

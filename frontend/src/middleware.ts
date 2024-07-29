9d4bf70b9efbb0352128eda11588d878f10bb233
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-07-29 15:09:33 +0300

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,4 +1,3 @@
-// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
@@ -14,17 +13,7 @@ export default function middleware(req: NextRequest) {
     pathname.startsWith('/_next/static') ||
     pathname.startsWith('/_next/image') ||
     pathname === '/favicon.ico' ||
-    pathname.startsWith('/left1.png') ||
-    pathname.startsWith('/left2.png') ||
-    pathname.startsWith('/Container.png') ||
-    pathname.startsWith('/hands.png') ||
-    pathname.startsWith('/right.png') ||
-    pathname.startsWith('/up.png') ||
-    pathname.startsWith('/star.png') ||
-    pathname.startsWith('/group.png') ||
-    pathname.startsWith('/house.png') ||
-    pathname.startsWith('/Pid.png') ||
-    pathname.startsWith('/bgBaby.png')
+    pathname.startsWith('/public')
   ) {
     return NextResponse.next();
   }

e98d78d9ad2f3fb3652aa7b6bda123258df9597d
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-07-29 15:05:25 +0300

    Update middleware.ts

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,3 +1,4 @@
+// middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
@@ -8,7 +9,23 @@ export default function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
 
   // Исключаем статические файлы и API из обработки middleware
-  if (pathname.startsWith('/api') || pathname.startsWith('/_next/static') || pathname.startsWith('/_next/image') || pathname === '/favicon.ico') {
+  if (
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/_next/static') ||
+    pathname.startsWith('/_next/image') ||
+    pathname === '/favicon.ico' ||
+    pathname.startsWith('/left1.png') ||
+    pathname.startsWith('/left2.png') ||
+    pathname.startsWith('/Container.png') ||
+    pathname.startsWith('/hands.png') ||
+    pathname.startsWith('/right.png') ||
+    pathname.startsWith('/up.png') ||
+    pathname.startsWith('/star.png') ||
+    pathname.startsWith('/group.png') ||
+    pathname.startsWith('/house.png') ||
+    pathname.startsWith('/Pid.png') ||
+    pathname.startsWith('/bgBaby.png')
+  ) {
     return NextResponse.next();
   }
 

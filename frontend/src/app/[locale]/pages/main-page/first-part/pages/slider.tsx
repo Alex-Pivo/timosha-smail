017779307934e96fb9581eef7904c2ca7e561dff
AlexseyPivoshencko <110694722+Alex-Pivo@users.noreply.github.com>
2024-07-29 22:34:26 +0300

    Update slider.tsx

--- frontend/src/app/[locale]/pages/main-page/first-part/pages/slider.tsx
+++ frontend/src/app/[locale]/pages/main-page/first-part/pages/slider.tsx
@@ -30,7 +30,7 @@ export default function Slider({ locale }: any) {
     try {
       let data;
       axios
-        .get(`https://timoshas-smile.org:8443/${localeValue}`)
+        .get(`https://timoshas-smile.org:8443/${localeValue}/`)
         .then((res) => {
           data = res.data;
           setState(data);

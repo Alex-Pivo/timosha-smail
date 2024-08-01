e313536de2189c264a390afdca4e45c2086aecab
cl9bix <yuraklyap1103@gmail.com>
2024-08-01 18:27:27 +0200

    fix

--- backend/djangoProject/news/models.py
+++ backend/djangoProject/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from ckeditor.fields import RichTextField
 
 class ChooseLanguage(models.TextChoices):
     UKRAINIAN = 'uk', 'Українська'
@@ -14,11 +15,10 @@ class Category(models.TextChoices):
     YOUR_HELP = 'your_help', 'Ваша допомога'
     LIFE_OF_FOND = 'life_of_fond', 'Життя фонду'
     INTERVIEW = 'interview', 'Інтерв’ю'
-
 class News(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     description = models.TextField(max_length=400, verbose_name='Опис')
-    content = models.TextField(verbose_name="Текст новини")
+    content = RichTextField(verbose_name="Текст новини")
     short_description = models.TextField(max_length=200, verbose_name='Скорочений текст')
     time_to_read = models.CharField(max_length=5, default='10', verbose_name="Час прочитання")
     video = models.URLField(verbose_name='Посилання на YouTube:', blank=True, null=True,default=' ')
